7092c7e4258d3ffde83fc153b03b52e465b42811
SHAMYA . <ShamyaJain>
2024-02-02 22:30:58 +0530

    first commit

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input() data: string = '';
+  userName: string = "Shamya";
+  verifyUser(data: string): boolean {
+    return data === this.userName;
+  }
+}
